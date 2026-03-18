d654d9b9a64c243cea03fda385b9d0b975e35885
maverick0721 <shivang0721gupta@gmail.com>
2026-03-18 12:31:48 +0000

    Created full quantum Transformer

---
+++ quantum/transformer/q_transformer.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+from quantum.transformer.q_transformer_layer import QTransformerLayer
+
+
+class QTransformer(nn.Module):
+
+    def __init__(self, input_dim=5, dim=16, num_layers=2):
+        super().__init__()
+
+        self.embedding = nn.Linear(input_dim, dim)
+
+        self.layers = nn.ModuleList([
+            QTransformerLayer(dim) for _ in range(num_layers)
+        ])
+
+        self.output = nn.Linear(dim, 1)
+
+    def forward(self, x):
+
+        x = self.embedding(x)
+
+        for layer in self.layers:
+            x = layer(x)
+
+        x = self.output(x)
+
+        return x
