196d300b87d1930fe525ebaa56cf42312834d50c
maverick0721 <shivang0721gupta@gmail.com>
2026-03-18 14:03:48 +0000

    Updated block to add zk proof & includes it in hash computation

--- core/blockchain/block.py
+++ core/blockchain/block.py
@@ -1,9 +1,9 @@
-from core.utils.utils import sha256, serialize, current_timestamp
+from core.utils import sha256, serialize, current_timestamp
 
 
 class Block:
 
-    def __init__(self, index, transactions, previous_hash):
+    def __init__(self, index, transactions, previous_hash, zk_proof=None):
 
         self.index = index
         self.timestamp = current_timestamp()
@@ -11,6 +11,8 @@ class Block:
         self.previous_hash = previous_hash
 
         self.nonce = 0
+        self.zk_proof = zk_proof  # NEW FIELD
+
         self.hash = self.compute_hash()
 
     def compute_hash(self):
@@ -20,7 +22,8 @@ class Block:
             "timestamp": self.timestamp,
             "transactions": [t.to_dict() for t in self.transactions],
             "previous_hash": self.previous_hash,
-            "nonce": self.nonce
+            "nonce": self.nonce,
+            "zk_proof": self.zk_proof  # INCLUDED IN HASH
         }
 
         return sha256(serialize(block_data))
